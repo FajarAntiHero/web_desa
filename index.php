5143b5c3751b8f48b40235adbeb9d9634eccd271
Sipaaw <silvanamayangsari1@gmail.com>
2025-10-19 13:37:04 +0700

    kerangka navbar

--- index.php
+++ index.php
@@ -5,8 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="static/css/preflight.css">
+    <link rel="stylesheet" href="static/css/navbar.css">
 </head>
 <body>
-    
+    <nav>
+        <div class="nav-icon">
+            <div><img src="" alt=""></div>
+            <p>Widi</p>
+        </div>
+        <div class="nav-list">
+            <a href="">Home</a>
+            <a href="">Product</a>
+            <a href="">News</a>
+        </div>
+        <div class="nav-contact">
+            <a href="">Contact</a>
+        </div>
+    </nav>
 </body>
 </html>
