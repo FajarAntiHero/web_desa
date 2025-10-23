9a230de1aa9daeec4d8f6b2a5465d9f3e68090a5
Mewo-991 <sarizqijaya@gmail.com>
2025-10-23 22:35:12 +0700

    jumbroton index + css

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="static/css/preflight.css">
+    <link rel="stylesheet" href="static/css/jumbotron.css">  <!-- link css jumbotron -->
 </head>
 <body>
-    
+    <!-- ini jumbroton Start -->
+    <div class="jumbotron">
+        <div class="jumbotron-content">
+            <h1>Selamat Datang di Sukasari Website</h1>
+            <p>Publikasi informasi bermanfaat untuk warga Desa Sukasari</p>
+        </div>
+    </div>
+    <!-- ini jumbotron stop -->
 </body>
 </html>
