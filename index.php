758d79083c64f1d46ddb298b959b2bedd8604dd1
FajarAntiHero <fajar030506.gmail.com>
2025-10-28 16:35:55 +0700

    adding library css

--- index.php
+++ index.php
@@ -3,10 +3,38 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Beranda | Sukasari Website</title>
     <link rel="stylesheet" href="static/css/preflight.css">
+    <link rel="stylesheet" href="static/css/library.css">
+    <link rel="stylesheet" href="static/css/navbar.css">
+    <link rel="stylesheet" href="static/css/footer.css">
+    <link rel="stylesheet" href="static/css/index.css">
 </head>
 <body>
-    
+    <nav>
+        <div class="container d-flex navbar-container">
+            <div>
+                <div></div>
+                <div>
+                    <p>Suweb</p>
+                </div>
+            </div>
+            <div>
+                <a href="">Beranda</a>
+                <a href="">Berita</a>
+                <a href="">Produk</a>
+            </div>
+            <div>
+                <a href="">Hubungi Kami</a>
+            </div>
+        </div>
+    </nav>
+    <header></header>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <footer></footer>
 </body>
 </html>
