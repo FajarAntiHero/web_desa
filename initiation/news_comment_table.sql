9a85417fb461c53cee7fe1e5b8a750a5975758da
Mewo-991 <sarizqijaya@gmail.com>
2025-11-21 23:10:57 +0700

    Update news_content & news_comment jadi LONGTEXT

--- initiation/news_comment_table.sql
+++ initiation/news_comment_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE news_comment (
     id CHAR(10) PRIMARY KEY,
     comment_time_send TIME NOT NULL,
-    comment VARCHAR(500)
+    comment LONGTEXT
 );
--- initiation/news_table.sql
+++ initiation/news_table.sql
@@ -5,7 +5,7 @@ news_release_date DATE NOT NULL,
 news_author VARCHAR(100) NOT NULL,
 news_reader BIGINT DEFAULT 0,
 news_likes BIGINT DEFAULT 0,
-news_content VARCHAR(2000),
+news_content LONGTEXT,
 news_img_path VARCHAR(255),
 news_comment CHAR(10) UNIQUE
 );
